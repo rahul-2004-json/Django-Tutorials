97c77e2ebfa241bd75840b4e9457b11303bfcbd9
rahul_004 <rahulbky2004@gmail.com>
2024-08-19 23:21:57 +0530

    19/08/24 -Created Models in Django framework -Learned how to make migrations of code to admin -Made connection between the models and the admin panel.

--- djangoTutorial/djangoTutorial/urls.py
+++ djangoTutorial/djangoTutorial/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 #Import views from current directory
 from . import views
 urlpatterns = [
@@ -23,8 +25,9 @@ urlpatterns = [
     path("",views.Home,name="home"),
     path("about/",views.about,name="about"),
     path("contact/",views.contact,name="contact"),
+
     # Below is an example of passing the control from main urls.py to a urls.py in another app
     # If someone hits route chai/ then the control will be transfered to the urls.py file present in chai
     path("chai/",include('chai.urls')),   # in include('chai.urls')  this is coming from registered app in settings.py file
     path("__reload__/", include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
