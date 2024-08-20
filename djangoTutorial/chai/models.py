cab930dd813de294beb2c73eeb120a461b11d152
_rahul_yadav_ <rahulbky2004@gmail.com>
2024-08-20 18:16:18 +0530

    20/08/24
    -Handle Models and URLS in Django 
    -Learned how to migrate models to admin dashboard once created

--- djangoTutorial/chai/models.py
+++ djangoTutorial/chai/models.py
@@ -15,6 +15,7 @@ class ChaiVariety(models.Model):
     image = models.ImageField(upload_to='chais/')
     date_added =  models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
-
+    description = models.TextField(default='')
+    
     def __str__(self):
         return self.name
