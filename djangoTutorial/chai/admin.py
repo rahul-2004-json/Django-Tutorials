4c857d8360dee985ca5397a345505898c20b198f
_rahul_yadav_ <rahulbky2004@gmail.com>
2024-08-21 17:44:38 +0530

    21/08/24
    -Learned about models and their relationships with each other
    -Types of relationships like One to One , Many to One , Many to Many

--- djangoTutorial/chai/admin.py
+++ djangoTutorial/chai/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety,ChaiReview,Store,ChaiCertificate
+
 # Register your models here.
-admin.site.register(ChaiVariety)
+class ChaiReviewInline(admin.TabularInline):  
+    model = ChaiReview   #we have injected ChaiReview model inline within an existing model
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('chai_varieties',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai','certificate_number')
+    
+admin.site.register(ChaiVariety,ChaiVarietyAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(ChaiCertificate,ChaiCertificateAdmin)
--- djangoTutorial/chai/models.py
+++ djangoTutorial/chai/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -19,3 +21,37 @@ class ChaiVariety(models.Model):
     
     def __str__(self):
         return self.name
+
+
+# One to many
+# When we use foreign key then it automatically detects that it is one to many relationship
+class ChaiReview(models.Model):
+    # Inside chai field I have passed chaiVariety model 
+    chai = models.ForeignKey(ChaiVariety,on_delete=models.CASCADE , related_name='reviews') #related name means , what this should be called in another table
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+
+
+# Many to Many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVariety,related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+#One to one 
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety,on_delete=models.CASCADE,related_name='certificate')
+    certificate_number = models.CharField(max_length = 100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.name.chai}'
